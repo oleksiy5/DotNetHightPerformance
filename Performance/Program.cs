cc60cd57406c945d409589044c9c8616509e79c4
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-10-10 07:12:00 +0200

    d

--- Performance/Program.cs
+++ Performance/Program.cs
@@ -12,24 +12,9 @@ namespace Performance
     {
         static void Main(string[] args)
         {
-            Random rn = new Random();
-            for (int ii = 0; ii < 5; ii++)
-            {
-                Console.WriteLine($"--Performance-- test_{ii}");
-
-                int count = 100000021;
-                int[] arr = new int[count];
-                for (int i = 0; i < count; ++i)
-                    arr[i] = i * rn.Next(0,5);
-
-                TestFasterArraySearch test = new TestFasterArraySearch();
-                test.Test_LINQ(arr);
-                test.Test_FOR(arr);
-                test.Test_UNSAFE(arr);
-                test.Test_UNSAFE_TASK(arr);
-                
-                Console.ReadLine();
-            }
+            TestDivision t = new TestDivision();
+            t.Test1();
+            t.Test2();
         }
     }
 
---
+++ Performance/TestDivision.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Performance
+{
+    class TestDivision
+    {
+        int _count = 9*1000*1000;
+        public void Test1()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < _count; i++)
+            {
+                int result = i / 2;
+            }
+            sw.Stop();
+            Console.WriteLine($"DIV (/): {sw.ElapsedMilliseconds} ms");
+        }
+
+
+        public void Test2()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < _count; i++)
+            {
+                int result = i >> 1;
+            }
+
+            sw.Stop();
+            Console.WriteLine($"SHIFT (>>): {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
